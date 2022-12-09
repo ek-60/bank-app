cda64e92d0f094c6955ad47b381bfb96f415cce2
ek-60 <eemeli.kyrola@gmail.com>
2022-12-09 16:00:00 +0200

    Update signUpForm.js
    added password box to signup form

--- src/components/loginPage/signUpForm.js
+++ src/components/loginPage/signUpForm.js
@@ -20,6 +20,14 @@ function SignUp() {
             placeholder="Your surename.."
           ></input>
         </div>
+        <div className="flex flex-row items-center h-16 p-2 gap-6">
+          <p className="w-32 text-white text-xl font-semibold">Password:</p>
+          <input
+            type="password"
+            className="w-72 text-xl p-2 bg-transparent text-white focus:text-white focus:border-b-red-400 focus:border-b-4 focus:outline-none"
+            placeholder="Your password.."
+          ></input>
+        </div>
       </div>
       <div className="flex items-center justify-center">
         <button className="w-32 h-14 p-2 rounded-lg bg-blue-400 text-xl uppercase font-bold tracking-wide">
